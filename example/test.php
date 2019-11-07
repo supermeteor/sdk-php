104dcdbd9a3bebd17e9f8fa33b5f91430e4667c5
Lawrence LAU <lncknight@gmail.com>
2019-11-07 09:45:51 +0800

    rename example folder

--- example/test.php
+++ example/test.php
@@ -4,10 +4,11 @@ require_once '../vendor/autoload.php';
 require_once '../Supermeteor.php';
 
 $secret = 'xxxxxxxxxxxxxx';
+$secret = '974a06a28c6c811c70d5b3b52dbb0776b07deb96';
 
 $sandbox = true;
-$fromPhone = '+85268888888';
-$toPhone = '+85269999999';
+$fromPhone = '+8615015935642';
+$toPhone = '+85264860659';
 
 try {
     $supermeteor = new \Supermeteor\Client($secret, $sandbox);
